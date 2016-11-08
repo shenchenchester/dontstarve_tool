455d026b5d2a8279b5e73bce03a4f10119450989
Chester <shenchenchester@gmail.com>
2016-11-09 00:42:53 +0800

    init commit

---
+++ backup.py
@@ -0,0 +1,128 @@
+import os
+import shutil
+from datetime import datetime
+import time
+import stat
+import save_reader as sr
+import json
+
+# SAVE_DIR = 'C:/Users/ld/Documents/Klei/DoNotStarveTogether'
+SAVE_DIR = '/Users/chester/Documents/Klei/DoNotStarveTogether'
+BACKUP_DIR = '/Users/chester/Documents/Klei/backups'
+INTERVAL = 8*60 # seconds
+
+def copytree(src, dst, symlinks = False, ignore = None):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        shutil.copystat(src, dst)
+    lst = os.listdir(src)
+    if ignore:
+        excl = ignore(src, lst)
+        lst = [x for x in lst if x not in excl]
+    for item in lst:
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if symlinks and os.path.islink(s):
+            if os.path.lexists(d):
+                os.remove(d)
+            os.symlink(os.readlink(s), d)
+            try:
+                st = os.lstat(s)
+                mode = stat.S_IMODE(st.st_mode)
+                os.lchmod(d, mode)
+            except:
+                pass # lchmod not available
+        elif os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+def remove(d):
+    if os.path.exists(d):
+        shutil.rmtree(d)
+        print 'removed', d
+
+def backup(dst=None, trunc=True):
+    if not dst:
+        t = datetime.now()
+        dirname = t.strftime('%y%m%d_%H%M%S')
+        dst = BACKUP_DIR + '/' + dirname
+
+    copytree(SAVE_DIR, dst)
+    print 'backup to', dst
+
+    # remove old backups
+    if trunc:
+        backups = get_backups()
+        print backups
+        if len(backups) > 10:
+            to_remove = backups[:len(backups) - 10]
+            for b in to_remove:
+                d = BACKUP_DIR +'/' + b
+                remove(d)
+
+def loop():
+    print 'running backup'
+    lasttime = time.time()
+    while True:
+        time.sleep(INTERVAL)
+        save_path = os.path.join(SAVE_DIR, 'client_save')
+        session = sr.load_saveindex(save_path, use_cache=False)
+        if session and session['time'] - lasttime > 0:
+            t = datetime.fromtimestamp(session['time'])
+            dirname = '{}_{}'.format(t.strftime('%y%m%d_%H%M%S'), session['summary']['days'])
+            dst = os.path.join(BACKUP_DIR, dirname)
+            backup(dst)
+            lasttime = session['time']
+        else:
+            print 'save not updated, skip backup'
+
+def get_backups():
+    if not os.path.exists(BACKUP_DIR):
+        return []
+    return sorted(os.listdir(BACKUP_DIR))
+
+def restore(b):
+    d = BACKUP_DIR +'/' + b
+    tmp = BACKUP_DIR + '/restore'
+    remove(tmp)
+    backup(dst=tmp, trunc=False)
+    shutil.rmtree(SAVE_DIR)
+    copytree(d, SAVE_DIR)
+    print 'restore', d
+
+def menu():
+    print '1. start backup every {:.1f} minutes\n\
+2. restore a previous backup\n\
+3. exit'.format(INTERVAL/60.0)
+    n = raw_input('input number(1 by default):')
+    if n=='1' or n == '':
+        loop()
+    elif n=='2':
+        backups = list(reversed(get_backups()))
+        for i, d in enumerate(backups):
+            print i+1, d
+            save_path = os.path.join(BACKUP_DIR, d, 'client_save')
+            session = sr.load_saveindex(save_path, use_cache=True)
+            print ' ', session['summary']['title']
+        k = raw_input('input number:')
+        print(k, len(backups))
+        to_restore = 0
+        try:
+            to_restore = backups[int(k)-1]
+        except:
+            print 'invalid input!'
+            return
+        restore(to_restore)
+    elif n=='3':
+        return
+    else:
+        print n
+        print 'invalid input!'
+
+def main():
+    menu()
+    print 'exit'
+    time.sleep(0.5)
+
+main()
---
+++ save_reader.py
@@ -0,0 +1,167 @@
+import base64
+import zlib
+from slpp import slpp as lua
+import os
+import json
+# root = '/Users/chester/Documents/Klei/DoNotStarveTogether/client_save'
+# root = '/Volumes/NO NAME/helper/save/161106_180518/client_save'
+# lua_exp = decompressed[7:]
+# data = lua.decode(lua_exp)
+
+def load_saveindex(root, use_cache=False):
+    current = None
+    if use_cache:
+        current = load_cache(root)
+        if current:
+            return current
+
+    s = read_file(root+'/saveindex')
+    data = lua.decode(s[18:])
+    for slot in data['slots']:
+        if slot.get('session_id'):
+            session_dir = os.path.join(root, 'session', slot['session_id'])
+            tmp = load_session(session_dir)
+            if tmp:
+                ot = tmp['summary']['title']
+                tmp['summary']['title'] = '{}\t{}'.format(ot, slot['server']['name'])
+                if not current:
+                    current = tmp
+                elif current['time']<tmp['time']:
+                    current = tmp
+    dump_cache(current, root)
+    return current
+
+def load_player_history(root):
+    decode(root+'/player_history')
+
+def load_session(session_dir):
+    if not os.path.exists(session_dir):
+        return
+    files = []
+    dirs = []
+    for f in os.listdir(session_dir):
+        if os.path.isfile(os.path.join(session_dir, f)):
+            files.append(f)
+        else:
+            dirs.append(f)
+    files = [f for f in files if f.isdigit()]
+    files.sort()
+    if len(files) == 0:
+        return
+    ws_path = os.path.join(session_dir, files[-1])
+    world = load_world_session(ws_path)
+    # players = world.get('snapshot', {}).get('players', [])
+    # if len(players)==0:
+    #     return
+    # dirs = [f for f in dirs if f[:-1] in players]
+    users = []
+    for user in dirs:
+        us_path = os.path.join(session_dir, user, files[-1])
+        if not os.path.exists(us_path):
+            return
+        tmp = load_user_session(us_path)
+        if tmp:
+            users.append(tmp)
+    summary = summary_for_session(world, users)
+    return {
+        'world':world,
+        'users':users,
+        'summary':summary,
+        'time':os.path.getmtime(ws_path)
+    }
+
+def read_file(path):
+    f = open(path, 'rb')
+    content = f.read()
+    f.close()
+    return content
+
+def write_file(content, path):
+    f = open(path, 'w+')
+    to_write = ''
+    if isinstance(content, dict):
+        to_write = json.dumps(content)
+    else:
+        to_write = content
+    f.write(to_write)
+    f.close()
+
+def dump_cache(session, path):
+    if session:
+        cache_path = os.path.join(path, 'cache')
+        write_file(session, cache_path)
+    else:
+        clear_cache(path)
+
+def load_cache(path):
+    cache_path = os.path.join(path, 'cache')
+    cache = None
+    if os.path.exists(cache_path):
+        content = read_file(cache_path)
+        try:
+            cache = json.loads(content)
+        except:
+            pass
+    return cache
+
+def clear_cache(path):
+    cache_path = os.path.join(path, 'cache')
+    if os.path.exists(cache_path):
+        os.remove(cache_path)
+
+def extract_table(content):
+    s = content.find('{')
+    if s<0:
+        return ''
+    content = content[s:]
+    n = 0
+    for i, c in enumerate(content):
+        if c == '{':
+            n += 1
+        elif c == '}':
+            n -= 1
+            if n==0:
+                break
+    return content[:i+1]
+
+def decode(path):
+    raw = read_file(path)
+    decoded = base64.b64decode(raw[11:])
+    decompressed = zlib.decompress(decoded[16:])
+    return decompressed
+
+def load_world_session(path):
+    s = read_file(path)
+    data = lua.decode(s[7:])
+    return data
+
+def load_user_session(path):
+    s = read_file(path)
+    s = extract_table(s)
+    data = lua.decode(s)
+    return data
+
+def summary_for_session(world, users):
+    if not world.get('world_network') or not world['world_network'].get('persistdata') or not world['world_network']['persistdata'].get('clock'):
+        return {
+            'days': 0,
+            'title': ''
+        }
+    clock = world['world_network']['persistdata']['clock']
+    str_day = '{} of Day {}'.format(clock['phase'], clock['cycles'])
+    seasons = world['world_network']['persistdata']['seasons']
+    str_season = '{} ({}/{})'.format(seasons['season'], seasons['elapseddaysinseason'], seasons['elapseddaysinseason']+seasons['remainingdaysinseason'])
+    str_user = ', '.join(['{} ({}{})'.format(user['prefab'], user['age'], user['data'].get('is_ghost', False) and ', DEAD' or '') for user in users])
+    line = '{}\t{}\t{}'.format(str_day, str_season, str_user)
+    return {
+        'days':clock['cycles'],
+        'title': line
+    }
+
+# decode(root+'/client_save/player_history')
+# decode(root+'/client_save/morgue')
+# decode(root+'/profile')
+
+# root = '/Users/chester/Documents/Klei/DoNotStarveTogether/client_save'
+# current = load_saveindex(root)
+# print current['summary']['title']
